3ea5814635c326fc8ff61778538d6b329db050bb
kennycallado <kennycallado@hotmail.com>
2024-06-15 10:30:00 +0200

    UPDATE devcontainer: add lvim installation

--- .devcontainer/config/post-create.sh
+++ .devcontainer/config/post-create.sh
@@ -6,10 +6,10 @@ PATH="${PATH}:${HOME}/.local/bin"
 
 # install neovim and lunarvim
 bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/rolling/utils/installer/install-neovim-from-release)
-# LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh) --no-install-dependencies -y
 
 # place configurations
 ln -s $PWD/.devcontainer/config/nvim $HOME/.config/nvim
-# ln -s $PWD/.devcontainer/config/lvim $HOME/.config/
+# ln -s $PWD/.devcontainer/config/lvim $HOME/.config/lvim
 
 exit 0
