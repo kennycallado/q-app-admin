40ab79f83050b15c34d949f834e786f6f2c84b8b
kennycallado <kennycallado@gmail.com>
2024-06-15 09:41:30 +0000

    ADD devcontainer: basic lvim configuration

---
+++ .devcontainer/config/lvim/config.lua
@@ -0,0 +1,85 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- vim options
+vim.opt.relativenumber = true
+vim.opt.spelllang = "es"
+vim.opt.scrolloff = 4
+
+-- keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<S-k>"] = "<Plug>RestNvim"
+
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+
+-- appearance
+lvim.transparent_window = true
+
+-- builtins config
+lvim.builtin.nvimtree.setup.view.side = "right"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- langs
+lvim.autocommands = {
+  {
+    { "BufEnter", "BufWinEnter" },
+    {
+      group = "lvim_user",
+      pattern = "*.php",
+      command = "set shiftwidth=4 tabstop=4",
+    },
+  },
+}
+
+-- plugins
+lvim.plugins = {
+  {
+    "rest-nvim/rest.nvim",
+    version = "v1.2.1",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("rest-nvim").setup({
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = "jq",
+            -- html = function(body)
+            --   return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+            -- end
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      })
+    end
+  },
+}
+
+-- after plugins
+
+
