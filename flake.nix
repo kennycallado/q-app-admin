f96d17aef3d7d8757d4d691c7f11bd6dffac3178
kennycallado <kennycallado@gmail.com>
2024-06-15 09:41:04 +0000

    FMT

--- flake.nix
+++ flake.nix
@@ -7,40 +7,42 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { nixpkgs , systems, ... }:
+  outputs = { nixpkgs, systems, ... }:
     let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-  in {
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
 
-    devShells = eachSystem (system: {
+      devShells = eachSystem (system: {
         default =
-        let
-        pkgs = import nixpkgs { inherit system; };
-        in pkgs.mkShell {
-        packages = with pkgs; [
-        php82
-        php82Packages.composer
-        # php82Extensions.xml
-        # php82Extensions.curl
-        # php82Extensions.pcov
-        # php82Extensions.mbstring
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          pkgs.mkShell {
+            packages = with pkgs; [
+              php82
+              php82Packages.composer
+              # php82Extensions.xml
+              # php82Extensions.curl
+              # php82Extensions.pcov
+              # php82Extensions.mbstring
 
-        nodejs_20
-        ];
+              nodejs_20
+            ];
 
-        shellHook = ''
-        echo "ready to rock! 🚀"
-        echo 
-        echo "=================="
-        echo `${pkgs.php82Packages.composer}/bin/composer --version`
-        echo "=================="
-        echo -e "node:"
-        echo `${pkgs.nodejs_20}/bin/node --version`
-        echo "=================="
-        '';
-        };
-    });
+            shellHook = ''
+              echo "ready to rock! 🚀"
+              echo 
+              echo "=================="
+              echo `${pkgs.php82Packages.composer}/bin/composer --version`
+              echo "=================="
+              echo -e "node:"
+              echo `${pkgs.nodejs_20}/bin/node --version`
+              echo "=================="
+            '';
+          };
+      });
 
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-  };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+    };
 }
