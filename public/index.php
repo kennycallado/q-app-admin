2c28c4a98dc92f0bfb6c64891866c6a441a97195
kennycallado <kennycallado@hotmail.com>
2024-06-15 08:04:29 +0200

    UPDATE a bit more complete public endpoint

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
-<?php phpinfo(); ?>
+<?php declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+use Slim\Views\TwigMiddleware;
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+// Add MethodOverride middleware
+$methodOverrideMiddleware = new MethodOverrideMiddleware();
+$app->add($methodOverrideMiddleware);
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Run
+$app->run();
