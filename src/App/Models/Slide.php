488ffcd17c928f9ddd5500730bd319e2139ce577
kennycallado <kennycallado@hotmail.com>
2024-07-21 17:41:06 +0200

    UPDATE slides: model, repo, controller

---
+++ src/App/Models/Slide.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\Models;
+
+/**
+ * Class slide
+ * @package App\Models
+ */
+class Slide
+{
+    public ?string $id;
+    public string $ref;
+    public string $title;
+    public array $elements;
+
+    /**
+     * @param string $ref
+     * @param string $title
+     * @param ?string[] $elements
+     * @param string|null $id
+     */
+    public function __construct(string $ref, string $title, ?array $elements, ?string $id = null)
+    {
+        $this->id = $id;
+        $this->ref = $ref;
+        $this->title = $title;
+        $this->elements = $elements ?? [];
+    }
+}
